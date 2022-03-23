542b06f1ec8566a0914212581346795d8d6fc87a
Yasin Silavi <ys.silavi@gmail.com>
2022-03-23 14:53:27 +0430

    refactor: write all the styles in vue components

--- src/components/share-module.vue
+++ src/components/share-module.vue
@@ -46,4 +46,59 @@ export default {
 };
 </script>
 
-<style src="@/styles/share-module.css" scoped />
+<style scoped>
+.share-module {
+  all: unset;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  flex-direction: column;
+  color: var(--share-modal-secondary-color);
+}
+
+.share-module__icon-box {
+  display: flex;
+  width: 4.5rem;
+  height: 4.5rem;
+  align-items: center;
+  border-radius: 50rem;
+  justify-content: center;
+  transition: ease 0.3s all;
+}
+
+.share-module__icon-box:hover + .share-module__title {
+  color: var(--share-modal-primary-color);
+}
+
+.share-module__icon-box--normal {
+  background-color: rgba(157, 157, 157, 0.04);
+}
+
+.share-module__icon-box--normal:hover {
+  color: var(--share-modal-primary-color);
+  background-color: var(--share-modal-primary-light-color);
+}
+
+.share-module__icon-box--outline {
+  border: 1px solid rgba(157, 157, 157, 0.16);
+}
+
+.share-module__icon-box--outline:hover {
+  color: var(--share-modal-primary-color);
+  background-color: var(--share-modal-primary-light-color);
+  border: 1px solid rgba(238, 108, 77, 0.08);
+}
+
+.share-module__icon {
+  font-size: 1.5rem;
+}
+
+.share-module__title {
+  font-weight: 600;
+  margin-top: 0.5rem;
+  font-size: 0.75rem;
+  line-height: 0.9375rem;
+  transition: ease 0.3s all;
+  color: var(--share-modal-secondary-color);
+}
+</style>
